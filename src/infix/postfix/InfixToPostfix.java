f2161231916996e2eca7b669909c196bc32ccd79
Shashank Bansal <shashank.bansal_cs19@gla.ac.in>
2021-02-24 11:54:03 +0530

    infixtoPostfix code

--- src/infix/postfix/InfixToPostfix.java
+++ src/infix/postfix/InfixToPostfix.java
@@ -5,7 +5,55 @@ import java.util.Stack;
 public class InfixToPostfix {
     public String infixToPostfix(String infixString){
         String postfix = "";
-               
+        //step1
+        Stack<Character> stack=new Stack<>();
+        stack.push('(');
+        infixString=infixString.concat(""+')');
+
+        //Step2
+        for (int i = 0; i < infixString.length(); i++) {
+            char character =infixString.charAt(i);
+            //Step3
+            if(Character.isAlphabetic(character)){
+                postfix=postfix.concat(""+ character);
+            }
+            //Step4
+            else if(character=='('){
+                 stack.push(character);
+
+            }
+            //Step5
+            else if(isOperator(character)){
+                if(stack.peek()!='('){
+                    while (!stack.empty()){
+                        if(precedence(stack.peek())>=precedence(character)){
+                            postfix=postfix.concat(""+stack.pop());
+                        }else {
+                            stack.push(character);
+                            break;
+
+                        }
+                    }
+                }
+                else {
+                    stack.push(character);
+
+                }
+            }
+            //Step6
+            else if(character==')'){
+                while(!stack.empty()){
+                    if(stack.peek()!='('){
+                        postfix=postfix.concat(""+stack.pop());
+
+                    }
+                    else {
+                        stack.pop();
+                        break;
+                    }
+                }
+            }
+        }
         return postfix;
     }
     private boolean isOperator(char character){
